279294888bc8d6cb424c87eaaa1bb2550fa74559
Tshegofatsho <tshegomotlatle87@gmail.com>
2025-08-26 10:05:00 +0200

    ✨add: guard gallivant

---
+++ MHA.Tests/Implementations/GuardGallivantTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using MHA.Implementations;
+
+namespace MHA.Tests.Implementations
+{
+    public class GuardGallivantTests
+    {
+        private List<List<char>> ToGrid(string[] rows)
+        {
+            var grid = new List<List<char>>();
+            foreach (var row in rows)
+            {
+                grid.Add(new List<char>(row.ToCharArray()));
+            }
+            return grid;
+        }
+
+        [Fact]
+        public void ThrowsIfNoStartingPoint()
+        {
+            var grid = ToGrid(new[]
+            {
+                "....",
+                "....",
+                "...."
+            });
+
+            Assert.Throws<ArgumentException>(() => new GuardGallivant(grid));
+        }
+
+        [Fact]
+        public void SimpleStraightLine_NoObstacles()
+        {
+            var grid = ToGrid(new[]
+            {
+                "....",
+                "..^.",
+                "....",
+                "...."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Walks straight up, marking positions until exits map
+            Assert.Equal(2, result); // starting + one move before exiting
+        }
+
+        [Fact]
+        public void StopsAtObstacleAndTurnsRight()
+        {
+            var grid = ToGrid(new[]
+            {
+                "....",
+                ".#..",
+                ".^..",
+                "...."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Starts at (2,1), moves up, encounters # at (1,1), turns right
+            Assert.True(result > 2);
+        }
+
+        [Fact]
+        public void PatrolTurnsMultipleTimes()
+        {
+            var grid = ToGrid(new[]
+            {
+                "....#....",
+                ".........",
+                "...#.....",
+                ".........",
+                ".....^..."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            Assert.True(result > 5); // should patrol around and mark several
+        }
+
+        [Fact]
+        public void DistinctPositionsCountedCorrectly()
+        {
+            var grid = ToGrid(new[]
+            {
+                ".....",
+                "..^..",
+                ".....",
+                "....."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Path: (1,2) -> (0,2). Marks 2 distinct cells.
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void ExitsMapGoingDown()
+        {
+            var grid = ToGrid(new[]
+            {
+                ".....",
+                ".....",
+                "..v.."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // From (2,2), goes down and immediately exits
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ExitsMapGoingLeft()
+        {
+            var grid = ToGrid(new[]
+            {
+                "<..."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Immediately exits, only starting pos
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ExitsMapGoingRight()
+        {
+            var grid = ToGrid(new[]
+            {
+                "...>"
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Immediately exits, only starting pos
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ComplexScenario_MatchesExpectedCount()
+        {
+            var grid = ToGrid(new[]
+            {
+                "....#.....",
+                ".........#",
+                "..........",
+                "..#.......",
+                ".......#..",
+                "..........",
+                ".#..^.....",
+                "........#.",
+                "#.........",
+                "......#..."
+            });
+
+            var guard = new GuardGallivant(grid);
+            int result = guard.Calculate();
+
+            // Example from problem description, expect > 10 positions visited
+            Assert.True(result > 10);
+        }
+    }
+}
---
+++ MHA/Implementations/GuardGallivant.cs
@@ -0,0 +1,146 @@
+﻿
+using System.Collections.Generic;
+using MHA.Interfaces;
+using MHA.Models.Enums;
+
+namespace MHA.Implementations;
+
+public class GuardGallivant : IBaseImplementation
+{
+    private readonly List<List<char>> _map;
+    (int X, int Y) _postion;
+    Directions _direction;
+
+    public GuardGallivant(List<List<char>> map)
+    {
+        _map = map;
+        _postion = FindArrow();
+
+        if (_postion == (-1,-1))
+        {
+            throw new ArgumentException("No Starting point specified, please specifiy with ^ on the map.");
+        }
+
+        _direction = FindDirection() ?? throw new ArgumentException("Direction of the starting position could not be determined");
+    }
+
+    private Directions? FindDirection()
+    {
+        return _map[_postion.X][_postion.Y] switch
+        {
+            '^' => (Directions?)Directions.Up,
+            '>' => (Directions?)Directions.Right,
+            '<' => (Directions?)Directions.Left,
+            'v' => (Directions?)Directions.Down,
+            _ => null,
+        };
+    }
+
+    private (int X, int Y) FindArrow()
+    {
+        for (int i = 0; i < _map.Count; i++)
+        {
+            for (int j = 0; j < _map[i].Count; j++)
+            {
+                if (_map[i][j] == '^' ||
+                    _map[i][j] == '>' || 
+                    _map[i][j] == '<' || 
+                    _map[i][j].ToString().Equals("v", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return (i, j);
+                }
+            }
+        }
+
+        return (-1, -1);
+    }
+
+    public int Calculate()
+    {
+        while (true)
+        {
+
+            if (((_postion.X + 1 == _map.Count && _direction == Directions.Down) || 
+                (_postion.X - 1 == -1 && _direction == Directions.Up) || 
+                (_postion.Y - 1 == -1 && _direction == Directions.Left) || 
+                (_postion.Y + 1 == _map[_postion.X].Count)) || 
+                (_postion.X == _map.Count && _postion.X == 0))
+            {
+                _map[_postion.X][_postion.Y] = 'X';
+                break;
+            }
+
+            switch (_direction)
+            {
+                case Directions.Up:
+                    if (_map[_postion.X - 1][_postion.Y] != '#')
+                    {
+                        _map[_postion.X][_postion.Y] = 'X';
+                        _postion.X--;
+                    }
+                    else
+                    {
+                        _direction = Directions.Right;
+                    }
+                    break;
+                case Directions.Down:
+                    if (_map[_postion.X + 1][_postion.Y] != '#')
+                    {
+                        _map[_postion.X][_postion.Y] = 'X';
+                        _postion.X++;
+                    }
+                    else
+                    {
+                        _direction = Directions.Left;
+                    }
+                    break;
+                case Directions.Left:
+                    if (_map[_postion.X][_postion.Y - 1] != '#')
+                    {
+                        _map[_postion.X][_postion.Y] = 'X';
+                        _postion.Y--;
+                    }
+                    else
+                    {
+                        _direction = Directions.Up;
+                    }
+                    break;
+                case Directions.Right:
+                    if (_map[_postion.X][_postion.Y + 1] != '#')
+                    {
+                        _map[_postion.X][_postion.Y] = 'X';
+                        _postion.Y++;
+                    }
+                    else
+                    {
+                        _direction = Directions.Down;
+                    }
+                    break;
+
+            }
+        }
+
+        int distinctPostions = CountDistinctPostions();
+        return distinctPostions;
+    }
+
+    private int CountDistinctPostions()
+    {
+        int distinctPostions = 0;
+
+        for (int i = 0; i < _map.Count; i++)
+        {
+            for (int j = 0;j < _map[i].Count; j++)
+            {
+                if (_map[i][j] == 'X')
+                {
+                    distinctPostions++;
+                }
+                Console.Write(_map[i][j]);
+            }
+            Console.WriteLine();
+        }
+
+        return distinctPostions;
+    }
+}
